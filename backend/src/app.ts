0d50e5afeab74a38f34270eb2bd2ddc16fbb85b1
symonmuchemi <simonkenya846@gmail.com>
2024-11-11 22:48:17 +0300

    refactor: rename index.ts to app.ts and integrate Prisma client with user routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,23 @@
 import express, { Application, Request, Response } from "express";
 import cookieParser from "cookie-parser";
+import { PrismaClient } from "@prisma/client";
+import userRoute from "./routes/user";
+import logger from 'morgan';
 import { config } from "dotenv";
 
 config();
 
 const app: Application = express();
-const PORT = process.env.PORT || 3000;
+const PORT: string | number = process.env.PORT || 3000;
+
+export const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(logger('dev'))
+
+app.use('/users', userRoute)
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
